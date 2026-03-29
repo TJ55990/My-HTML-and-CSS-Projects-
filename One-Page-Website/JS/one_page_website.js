e11c6a043a2a9246e0058bd0931b75a1425bdc74
TJ55990 <puneetsandhu.mail011@gmail.com>
2026-03-29 17:05:39 -0400

    Update one_page_website.js

--- One-Page-Website/JS/one_page_website.js
+++ One-Page-Website/JS/one_page_website.js
@@ -1,1 +1,18 @@
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
 
+images.forEach((img) => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "block";
+
+        // Replace small image with large version
+        let largeImg = img.src.replace("-small", "");
+        lightboxImg.src = largeImg;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
